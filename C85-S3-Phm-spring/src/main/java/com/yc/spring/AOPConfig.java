ff89048779ad2fd260a70d0b03d17cf00244e6e0
Administrator <Administrator@SKY-20190331DAB>
2020-08-14 15:00:44 +0800

    银行业务测试

---
+++ C85-S3-Phm-spring/src/main/java/com/yc/spring/AOPConfig.java
@@ -0,0 +1,29 @@
+package com.yc.spring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+import com.yc.spring.bean.Person;
+
+@Configuration
+@ComponentScan("com.yc.spring")
+/** 
+ * AOP 不是Spring 提供的概念
+ * Spring 对 AOP的依赖 AspectJ 框架
+ */
+@EnableAspectJAutoProxy   // 对应  <aop:aspectj-autoproxy/>
+public class AOPConfig {
+	
+	@Bean  // 未指定 id 方法名就是 id
+	public Person getPerson() {
+		return new Person();
+	}
+	
+	@Bean
+	public Hello getHello() {
+		return new Hello();
+	}
+
+}
---
+++ C85-S3-Phm-spring/src/main/java/com/yc/spring/Hello.java
@@ -0,0 +1,8 @@
+package com.yc.spring;
+
+public class Hello {
+
+	public void sayhello(){
+		System.out.println("ÄãºÃ");
+	}
+}
