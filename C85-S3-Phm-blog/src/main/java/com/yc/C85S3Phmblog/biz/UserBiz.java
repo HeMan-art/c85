5857047b6565185015d22b89f458a0dca4caedec
Administrator <Administrator@SKY-20190331DAB>
2020-08-25 08:39:40 +0800

    实现注册

---
+++ C85-S3-Phm-blog/src/main/java/com/yc/C85S3Phmblog/biz/UserBiz.java
@@ -0,0 +1,36 @@
+package com.yc.C85S3Phmblog.biz;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.yc.C85S3Phmblog.bean.User;
+import com.yc.C85S3Phmblog.dao.UserMapper;
+
+
+@Service
+public class UserBiz {
+
+	@Resource
+	private UserMapper uMapper;
+
+	public void register(User user) throws BizException {
+		// 可以忽略字段验证
+		// 同名验证
+		if (uMapper.countByAccount(user.getAccount()) > 0) {
+			throw new BizException("该用户已经存在!");
+		}
+		uMapper.insert(user);
+	}
+
+	public User login(User user) throws BizException {
+		// 可以忽略字段验证
+		// 同名验证
+		User dbuser = uMapper.selectByLogin(user);
+		if (dbuser == null) {
+			throw new BizException("用户名或密码错误!");
+		}
+		return dbuser;
+	}
+
+}
