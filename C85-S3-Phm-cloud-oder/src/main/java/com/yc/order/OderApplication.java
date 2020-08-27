10fc59bbb2b5c87e6fd898b619dea100ee4af8cc
Administrator <Administrator@SKY-20190331DAB>
2020-08-27 20:21:46 +0800

    通过远程服务器名远程调用

--- C85-S3-Phm-cloud-oder/src/main/java/com/yc/order/OderApplication.java
+++ C85-S3-Phm-cloud-oder/src/main/java/com/yc/order/OderApplication.java
@@ -1,11 +1,14 @@
-package com.yc.oder;
+package com.yc.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class OderApplication implements WebMvcConfigurer {
 
@@ -16,6 +19,9 @@ public class OderApplication implements WebMvcConfigurer {
 	/**
 	 * 	定义 RestTemplate  Bean
 	 */
+	
+	
+	@LoadBalanced
 	@Bean
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
--- C85-S3-Phm-cloud-oder/src/main/java/com/yc/order/web/OrderAction.java
+++ C85-S3-Phm-cloud-oder/src/main/java/com/yc/order/web/OrderAction.java
@@ -1,4 +1,4 @@
-package com.yc.oder.web;
+package com.yc.order.web;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
@@ -8,21 +8,26 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
-public class OderAction {
+public class OrderAction {
 	
 	@Resource
 	private RestTemplate restTemplate;
 
-	@GetMapping("oder")
+	@GetMapping("order")
 	public String order(HttpServletRequest req) {
 		return String.format("server:order ; ip:%s ; port:%s",
 				req.getLocalAddr(),
 				req.getLocalPort());
 	}
 	
+	/**
+	 * 	127.0.0.1:8002 ==> 订单地址
+	 * 	浏览器测试地址: http://127.0.0.1:8002/user
+	 * 				http://order/user  
+	 */
 	@GetMapping("user")
 	public String user() {
-		String url="http://127.0.0.1:8001/user";
+		String url="http://user/user"; // 系统内部的远程调用地址
 		String res = restTemplate.getForObject(url, String.class);
 		return res;
 	}
