f8d20a1eb78b6e1762d8d0cef09114fdc080a41d
Administrator <Administrator@SKY-20190331DAB>
2020-08-27 11:43:48 +0800

    实现服务远程互访

--- C85-S3-Phm-cloud-oder/src/main/java/com/yc/oder/web/OderAction.java
+++ C85-S3-Phm-cloud-oder/src/main/java/com/yc/oder/web/OderAction.java
@@ -1,14 +1,30 @@
 package com.yc.oder.web;
 
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class OderAction {
+	
+	@Resource
+	private RestTemplate restTemplate;
 
 	@GetMapping("oder")
-	public String order() {
-		return "oder";
+	public String order(HttpServletRequest req) {
+		return String.format("server:order ; ip:%s ; port:%s",
+				req.getLocalAddr(),
+				req.getLocalPort());
+	}
+	
+	@GetMapping("user")
+	public String user() {
+		String url="http://127.0.0.1:8001/user";
+		String res = restTemplate.getForObject(url, String.class);
+		return res;
 	}
 
 }
